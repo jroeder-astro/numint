cda7b102e63baf5ed06cf524ed6d8ce76424b9e0
Jan <34909681+badhatharry@users.noreply.github.com>
2017-12-28 11:57:13 +0100

    Add files via upload

--- numint.c
+++ numint.c
@@ -7,7 +7,6 @@
 // LIBRARY FOR NUMERICAL INTEGRATION
 
 
-
 double gaussian(double x, void *params){
 
   double *par = (double *)params;
@@ -17,12 +16,13 @@ double gaussian(double x, void *params){
   return 1/(sigma*sqrt(2*M_PI))*exp(-pow(((x-mu)/sigma), 2)/2);
 }
 
+
 double integration_1(double a, double b, void *params, double (*f)(double, void *)){
                                   // HOW DOES ONE PUT THE PARAMETERS IN HERE???   -> this apparently works lol  
   double *p=(double*)params;  
 
   double N=4;
-  double h=(fabs(a)+fabs(b))/N;
+  double h=(b-a)/N;
 
   // left Riemann sum
   double L=(*f)(a, p);                    // this bitch is the reason you gotta start with a+h in the for loop
@@ -38,33 +38,32 @@ double integration_1(double a, double b, void *params, double (*f)(double, void 
   }
   printf("Right sum of function is %f\n", h*R); 
 
-}
-
-
-/*   // works just fine!
-double f(double x){ return x*x; }         //   1/(sqrt(2*M_PI))*exp(-pow(x,2)/2); }
-
-double integration_1(double a, double b, double (*f)(double)){
-
-  // Riemann left sum
-  double N=4;
-  double h=(fabs(a)+fabs(b))/N;
-  printf("h=%f\n", h);
-  double T=(*f)(a);
+  // trapezoidal rule
+  double T=(*f)(a,p)+(*f)(b,p);
   for (double i=a+h; i<=b-h; i+=h){
-  	T+=(*f)(i);} 
-  printf("Left sum of function is %f\n", h*T);
-}
-*/
+	T+=2*(*f)(i, p);
+  }
+  printf("Trapezoidal int. of function is %f\n", h/2*T); 
 
+  // Simpson's rule
+  double S=(*f)(a,p)+(*f)(b,p);
+  for (double i=a+h; i<=b-h; i+=h){
+  	S+=2*(*f)(i, p);
+  }
+  for (double i=a+h/2; i<=b-h/2; i+=h){
+	S+=4*(*f)(i, p);
+  }
+  printf("Simpson's rule gives us %f\n", h/6*S); 
+
+}
+
+ 
 int main(){
   
   double x;
   double p[2]={0., 1.}; // array with mu and sigma
   gaussian(x, p);
-  // printf("it is %f\n", gaussian(x,p));
   integration_1(-1., 1., p, gaussian);
-
-  // integration_1(0, 2., f);
+  
 }
 
